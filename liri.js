211ecd7eacf104aac98629a60e5327bc39737d69
Dalton Duncan <daltonduncan44@gmail.com>
2018-08-13 19:16:49 -0400

    figured out .env and fixed undefine

--- liri.js
+++ liri.js
@@ -1,42 +1,51 @@
-/// requires to packages and keys
+
+/// requests and getting global varible keys
 var fs = require('fs'); 
 var request = require('request');
-require("dotenv").config();
+var dotenv = require("dotenv").config();
 var keys = require("./keys.js");
 var Twitter = require("twitter");
 var Spotify = require('node-spotify-api');
-//grab using inputs
+
+// Grabs the command from the terminal
 var command = process.argv[2];
 var searchValue = "";
 
-// turn inputs into a string 
+// Puts together the search value into one string
 for (var i = 3; i < process.argv.length; i++) {
     searchValue += process.argv[i] + " ";
 };
 
+// Error Functions 
+function errorFunction(respError) {
+    if (respError) {
+        return console.log("Error occured: ", respError);
+     }
+};
 
-// start twitter functions
-//make sure its last twenty tweets
+
+
+// -------------------- Twitter my-tweets ----------------------------
 function getTweets() {
 
     // Accesses Twitter Keys
     var client = new Twitter(keys.twitter); 
-    var params = "DaltonDDevs";
-       
-        
+    var params = {
+        screen_name: 'DaltonDDevs',
+        count: 20
+        };
 
-    client.get('statuses/user_timeline', params, function( tweets, response) {
-
-      
+    client.get('statuses/user_timeline', params, function(respError, tweets, response) {
 
 
-        console.log("\n----------------------------\n");
+
+        console.log("\n--------------Tweets :] --------------\n");
 
         for (i = 0; i < tweets.length; i++) {
             console.log(i + 1 + ". Tweet: ", tweets[i].text);
 
             // For alingment once the number of the tweet is 10 or higher
-            if (i + 1 > 20) {
+            if (i + 1 > 9) {
                 console.log("    Tweeted on: ", tweets[i].created_at + "\n");
             } else {
                 console.log("   Tweeted on: ", tweets[i].created_at + "\n");
@@ -47,21 +56,28 @@ function getTweets() {
 
         console.log("--------------------------------------------------\n");
 
-
+        
     });
 };
 
+
+
+// <<<<<<<<<<<<<<<<< Main Switch Case >>>>>>>>>>>>>>>>>>>>
+
+// Runs corresponding function based on user command
 switch (command) {
     case "my-tweets":
         getTweets();
         break;
+    case "spotify-this-song":
+        searchSong(searchValue);
+        break;
+    case "movie-this":
+        searchMovie(searchValue);
+        break;
+    case "do-what-it-says":
+        randomSearch();
+        break;
+    default:
+        console.log("\nI'm sorry, " + command + " is not a command that I recognize. Please try one of the following commands: \n\n  1. For a random search: node liri.js do-what-it-says \n\n  2. To search a movie title: node liri.js movie-this (with a movie title following) \n\n  3. To search Spotify for a song: node liri.js spotify-this-song (*optional number for amount of returned results) (specify song title)\n     Example: node liri.js spotify-this-song 15 Candle in the Wind\n\n  4. To see the last 20 of Aidan Clemente's tweets on Twitter: node liri.js my-tweets \n");
 };
-//start spotify functions
-// make sure to get artist, name,link, album 
-
-
-//start movie this functions 
-//get title year, rating, RT rating, country, language, plot, actors
-
-
-//start liri do what it says
