02b6205756f2ea8b0f026b1e9cd7624bf5caf403
Dalton Duncan <daltonduncan44@gmail.com>
2018-08-13 19:35:58 -0400

    spotify working

--- liri.js
+++ liri.js
@@ -59,8 +59,56 @@ function getTweets() {
         
     });
 };
+// ======================= Spotify spotify-this-song ============================
+function searchSong(searchValue) {
+
+    // Default search value if no song is given
+    if (searchValue == "") {
+        searchValue = "The Sign Ace of Base";
+    }
+
+    // Accesses Spotify keys  
+    var spotify = new Spotify(keys.spotify);
+
+    var searchLimit = "5";
+
+    // Allows the user to input the number of returned spotify results, defaults 1 return if no input given
+    ////if (isNaN(parseInt(process.argv[3])) == false) {
+       // searchLimit = process.argv[3];
+
+        //console.log("\nYou requested to return: " + searchLimit + " songs");
+        
+        // Resets the searchValue to account for searchLimit
+        //searchValue = "";
+        //for (var i = 4; i < process.argv.length; i++) {        
+        //    searchValue += process.argv[i] + " ";
+       // };
+
+ //   } else {
+   //     console.log("\nFor more than 1 result, add the number of results you would like to be returned after spotify-this-song.\n\nExample: if you would like 3 results returned enter:\n     node.js spotify-this-song 3 Kissed by a Rose")
+     //   searchLimit = 1;
+    //}
+    // Searches Spotify with given values
+    spotify.search({ type: 'track', query: searchValue, limit: searchLimit }, function(respError, response) {
 
 
+
+        var songResp = response.tracks.items;
+
+        for (var i = 0; i < songResp.length; i++) {
+            console.log("\n=============== Spotify Search Result "+ (i+1) +" ===============\n");
+            console.log(("Artist: " + songResp[i].artists[0].name));
+            console.log(("Song title: " + songResp[i].name));
+            console.log(("Album name: " + songResp[i].album.name));
+            console.log(("URL Preview: " + songResp[i].preview_url));
+            console.log("\n=========================================================\n");
+
+            fs.appendFile("log.txt", "\n========= Result "+ (i+1) +" =========\nArtist: " + songResp[i].artists[0].name + "\nSong title: " + songResp[i].name + "\nAlbum name: " + songResp[i].album.name + "\nURL Preview: " + songResp[i].preview_url + "\n=============================\n", errorFunction());
+        }
+
+        fs.appendFile("log.txt","-----Spotify Log Entry End-----\n\n", errorFunctionEnd());
+    })
+};
 
 // <<<<<<<<<<<<<<<<< Main Switch Case >>>>>>>>>>>>>>>>>>>>
 
