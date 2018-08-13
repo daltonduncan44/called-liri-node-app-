6be763d311a3de4d1f3090d07c69858dca3e2007
Dalton Duncan <daltonduncan44@gmail.com>
2018-08-13 15:40:06 -0400

    psuedo coding

---
+++ liri.js
@@ -0,0 +1,23 @@
+/// requires to packages and keys
+var fs = require('fs'); 
+var request = require('request');
+var dotenv = require("dotenv").config();
+var keys = require("./keys.js");
+var Twitter = require("twitter");
+var Spotify = require('node-spotify-api');
+//grab using inputs
+// turn inputs into a string 
+//create error/ 200
+
+// start twitter functions
+//make sure its last twenty tweets
+
+//start spotify functions
+// make sure to get artist, name,link, album 
+
+
+//start movie this functions 
+//get title year, rating, RT rating, country, language, plot, actors
+
+
+//start liri do what it says
